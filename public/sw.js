73db589c2a979385b1e58a5ef4ffaf3731adbfeb
PranitK <Pranit.Khadilkar@talentica.com>
2023-04-12 09:58:00 +0530

    added logic to remove the unused cache after service workers activation

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
+const CACHE_STATIC_NAME = 'static-v3'
+const CACHE_DYNAMIC_NAME = 'dynamic'
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static')
+    caches.open(CACHE_STATIC_NAME)
       .then((cache) => {
         console.log('[Service Worker] Precaching App shell')
         cache.addAll([
@@ -29,7 +31,18 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
-  return self.clients.claim();
+  event.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map(key => {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+  )
+  return self.clients.claim('');
 });
 
 self.addEventListener('fetch', function(event) {
@@ -41,12 +54,13 @@ self.addEventListener('fetch', function(event) {
         } else {
           return fetch(event.request)
             .then(res => {
-              return caches.open('dynamica')
+              return caches.open(CACHE_DYNAMIC_NAME)
                 .then(cache => {
                   cache.put(event.request.url, res.clone())
                   return res
                 })
             })
+            .catch(err => {})
         }
       })
   );
