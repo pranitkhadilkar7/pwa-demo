e4d6fe7db703071b76d56d557b45a0cefc595eac
PranitK <Pranit.Khadilkar@talentica.com>
2023-04-22 18:16:15 +0530

    adding cache first for the static files

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,21 @@
-const CACHE_STATIC_NAME = 'static-v8'
-const CACHE_DYNAMIC_NAME = 'dynamic-v3'
+const CACHE_STATIC_NAME = 'static-v9'
+const CACHE_DYNAMIC_NAME = 'dynamic-v4'
+const STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+]
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -7,22 +23,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_STATIC_NAME)
       .then((cache) => {
         console.log('[Service Worker] Precaching App shell')
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/offline.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/promise.js',
-          '/src/js/fetch.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ])
+        cache.addAll(STATIC_FILES)
         // cache.add('/')
         // cache.add('/index.html')
         // cache.add('/src/js/app.js')
@@ -59,6 +60,10 @@ self.addEventListener('fetch', (event) => {
             })
         })
     )
+  } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) {
+    event.respondWith(
+      caches.match(event.request)
+    );
   } else {
     event.respondWith(
       caches.match(event.request)
@@ -77,7 +82,9 @@ self.addEventListener('fetch', (event) => {
               .catch(err => {
                 return caches.open(CACHE_STATIC_NAME)
                   .then(cache => {
-                    return cache.match('/offline.html')
+                    if (event.request.url.indexOf('/help') > -1) {
+                      return cache.match('/offline.html')
+                    }
                   })
               })
           }
